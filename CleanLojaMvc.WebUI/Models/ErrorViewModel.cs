c16e38200e2f74aaadb3dc6989828fc1d8202625
phillrog <phillrog@hotmail.com>
2023-01-26 18:57:22 -0300

    Projetos

---
+++ CleanLojaMvc.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CleanLojaMvc.WebUI.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
