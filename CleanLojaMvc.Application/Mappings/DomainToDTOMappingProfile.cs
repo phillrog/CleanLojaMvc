b2f0b32753c4145c136eaccea3e393e4e6580961
phillrog <phillrog@hotmail.com>
2023-02-01 17:19:48 -0300

    Configuração automapper das dtos e modelos

---
+++ CleanLojaMvc.Application/Mappings/DomainToDTOMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using CleanLojaMvc.Application.DTOs;
+using CleanLojaMvc.Domain.Entities;
+
+namespace CleanLojaMvc.Application.Mappings
+{
+    public class DomainToDTOMappingProfile : Profile
+    {
+        public DomainToDTOMappingProfile()
+        {
+            CreateMap<Product, ProductDTO>().ReverseMap();
+            CreateMap<Category, CategoryDTO>().ReverseMap();
+        }
+    }
+}
