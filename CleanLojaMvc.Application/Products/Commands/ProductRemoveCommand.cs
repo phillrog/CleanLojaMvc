adc7717d626aca4ab382f841db806bfb31a72e01
phillrog <phillrog@hotmail.com>
2023-02-02 18:03:13 -0300

    Criação do comando product remove

---
+++ CleanLojaMvc.Application/Products/Commands/ProductRemoveCommand.cs
@@ -0,0 +1,16 @@
+﻿using CleanLojaMvc.Domain.Entities;
+using MediatR;
+
+namespace CleanLojaMvc.Application.Products.Commands
+{
+    public class ProductRemoveCommand : IRequest<Product>
+    {
+        public ProductRemoveCommand(int id)
+        {
+            Id = id;
+        }
+
+        public int Id { get; set; }
+        
+    }
+}
