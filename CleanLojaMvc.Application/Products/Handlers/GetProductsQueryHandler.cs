7349d274df451bbacf00d2b1696dea848ae17813
phillrog <phillrog@hotmail.com>
2023-02-02 18:06:18 -0300

    Implementação do query no handler product  lista

---
+++ CleanLojaMvc.Application/Products/Handlers/GetProductsQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using CleanLojaMvc.Application.Products.Queries;
+using CleanLojaMvc.Domain.Entities;
+using CleanLojaMvc.Domain.Interfaces;
+using MediatR;
+
+namespace CleanLojaMvc.Application.Products.Handlers
+{
+    public class GetProductsQueryHandler : IRequestHandler<GetProductsQuery, IEnumerable<Product>>
+    {
+        private readonly IProductRepository _productRepository;
+
+        public GetProductsQueryHandler(IProductRepository productRepository)
+        {
+            _productRepository = productRepository ?? throw new ArgumentNullException(nameof(ProductUpdateCommandHandler));
+        }
+
+        public async Task<IEnumerable<Product>> Handle(GetProductsQuery request, CancellationToken cancellationToken)
+        {
+            return await _productRepository.GetProductsAsync();
+        }
+    }
+}
