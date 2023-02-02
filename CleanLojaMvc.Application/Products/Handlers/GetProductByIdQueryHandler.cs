1715d06177d67932cc8f7b38e0d1849f76632db4
phillrog <phillrog@hotmail.com>
2023-02-02 18:06:24 -0300

    Implementação do query no handler product  by id

---
+++ CleanLojaMvc.Application/Products/Handlers/GetProductByIdQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using CleanLojaMvc.Application.Products.Queries;
+using CleanLojaMvc.Domain.Entities;
+using CleanLojaMvc.Domain.Interfaces;
+using MediatR;
+
+namespace CleanLojaMvc.Application.Products.Handlers
+{
+    public class GetProductByIdQueryHandler : IRequestHandler<GetProductByIdQuery, Product>
+    {
+        private readonly IProductRepository _productRepository;
+
+        public GetProductByIdQueryHandler(IProductRepository productRepository)
+        {
+            _productRepository = productRepository ?? throw new ArgumentNullException(nameof(ProductUpdateCommandHandler));
+        }
+
+        public async Task<Product> Handle(GetProductByIdQuery request, CancellationToken cancellationToken)
+        {
+            return await _productRepository.GetByIdAsync(request.Id);
+        }
+    }
+}
