4a29ef4a3c58f43e245c2ad96f113d430d387a04
phillrog <phillrog@hotmail.com>
2023-02-02 18:05:50 -0300

    Implementação do comando no handler product remove

---
+++ CleanLojaMvc.Application/Products/Handlers/ProductRemoveCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using CleanLojaMvc.Application.Products.Commands;
+using CleanLojaMvc.Domain.Entities;
+using CleanLojaMvc.Domain.Interfaces;
+using MediatR;
+
+namespace CleanLojaMvc.Application.Products.Handlers
+{
+    public class ProductRemoveCommandHandler : IRequestHandler<ProductRemoveCommand, Product>
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductRemoveCommandHandler(IProductRepository productRepository)
+        {
+            _productRepository = productRepository ?? throw new ArgumentNullException(nameof(ProductUpdateCommandHandler));
+        }
+
+        public async Task<Product> Handle(ProductRemoveCommand request, CancellationToken cancellationToken)
+        {
+            var product = await _productRepository.GetByIdAsync(request.Id);
+
+            if (product == null)
+            {
+                throw new ApplicationException("Entity could not be found.");
+            } 
+            
+            return await _productRepository.RemoveAsync(product);
+        }
+    }
+}
