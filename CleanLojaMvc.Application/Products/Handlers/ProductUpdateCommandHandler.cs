f3766d20712826b5fa518dfe2571f84bf65e8b8b
phillrog <phillrog@hotmail.com>
2023-02-02 18:05:41 -0300

    Implementação do comando no handler product update

---
+++ CleanLojaMvc.Application/Products/Handlers/ProductUpdateCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using CleanLojaMvc.Application.Products.Commands;
+using CleanLojaMvc.Domain.Entities;
+using CleanLojaMvc.Domain.Interfaces;
+using MediatR;
+
+namespace CleanLojaMvc.Application.Products.Handlers
+{
+    public class ProductUpdateCommandHandler : IRequestHandler<ProductUpdateCommand, Product>
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductUpdateCommandHandler(IProductRepository productRepository)
+        {
+            _productRepository = productRepository ?? throw new ArgumentNullException(nameof(ProductUpdateCommandHandler));
+        }
+
+        public async Task<Product> Handle(ProductUpdateCommand request, CancellationToken cancellationToken)
+        {
+            var product = await _productRepository.GetByIdAsync(request.Id);
+
+            if (product == null)
+            {
+                throw new ApplicationException("Entity could not be found.");
+            } 
+
+            product.Update(request.Name, request.Description, request.Price, request.Stock, request.Image, request.CategoryId);
+
+            return await _productRepository.UpdateAsync(product);
+        }
+    }
+}
