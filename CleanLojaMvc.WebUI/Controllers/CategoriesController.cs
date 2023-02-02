a2ce1412ac723e6c0fa20b439c17c2de4a217f9a
phillrog <phillrog@hotmail.com>
2023-02-02 18:07:16 -0300

    Criação do controller catetories

---
+++ CleanLojaMvc.WebUI/Controllers/CategoriesController.cs
@@ -0,0 +1,21 @@
+﻿using CleanLojaMvc.Application.Insterfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanLojaMvc.WebUI.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly ICategoryService _categoryService;
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var categories = await _categoryService.GetCategories();
+            return View(categories);
+        }
+    }
+}
