a921cb8f1b85b06cf813e8f1226a01e04675799e
phillrog <phillrog@hotmail.com>
2023-02-04 18:41:34 -0300

    Remoção do error e da action

--- CleanLojaMvc.WebUI/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace CleanLojaMvc.WebUI.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
