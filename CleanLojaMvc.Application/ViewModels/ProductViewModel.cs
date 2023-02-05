e8aa8b0f1384ed79b6f0439998b5dfb634ec7039
phillrog <phillrog@hotmail.com>
2023-02-05 16:36:39 -0300

    Menus

---
+++ CleanLojaMvc.Application/ViewModels/ProductViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace CleanLojaMvc.Application.ViewModels
+{
+    public class ProductViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public string Image { get; set; }
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
