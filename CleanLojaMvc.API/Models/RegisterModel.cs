7f1a5a22f5f8d3d2d26f07dd47fa9298f03d5d77
phillrog <phillrog@hotmail.com>
2023-02-11 18:53:49 -0300

    Criação do register model

---
+++ CleanLojaMvc.API/Models/RegisterModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CleanLojaMvc.API.Models
+{
+    public class RegisterModel
+    {
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid format email")]
+        public string Email { get; set; }
+
+        [Required()]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display( Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "Password don´t match")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+}
